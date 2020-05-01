b66ec0ec3f4ec54102e9232b2d0b31dcf9ea1129
plifis <nikich85@inbox.ru>
2020-05-01 17:31:53 +0400

    3.3. Разбор первой программы. [#283361]

---
+++ src/StudentInfo.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main (String[] args){
+System.out.println("Nikita Zhulyabin");
+System.out.println("16/04/1985");
+
+    }
+}
---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main (String [] args){
+        int x = 1;
+        for (int y = 1; y<=9;y++ )
+        {
+            System.out.println("1*" + y + "=" + x*y);
+        }
+    }
+}
