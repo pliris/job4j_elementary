6a636ce560d279024adabf33142b1a4d4789ecc3
plifis <nikich85@inbox.ru>
2020-05-02 20:40:55 +0400

    9.2. Checkstyle[#283356]

--- src/StudentInfo.java
+++ src/StudentInfo.java
@@ -1,5 +1,5 @@
 public class StudentInfo {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 System.out.println("Nikita Zhulyabin");
 System.out.println("16/04/1985");
 
--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -1,11 +1,10 @@
 package ru.job4j;
 
 public class Multiple {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         int x = 1;
-        for (int y = 1; y<=9;y++ )
-        {
-            System.out.println("1*" + y + "=" + x*y);
+        for (int y = 1; y <= 9; y++) {
+            System.out.println("1*" + y + "=" + x * y);
         }
     }
 }
