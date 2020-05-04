7b3b5a583edb8e6e81b95926f737dea937014fb9
pliris <nikich85@inbox.ru>
2020-05-04 22:28:12 +0300

    5.7. Ипотека.[#283393]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(double amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100;
+        while (amount >= 0) {
+            amount += (amount * percent) - salary;
+            year++;
+        }
+        return year;
+    }
+}
