888ba4ee5f9fbad9a5be02864ffcb94b7ea8851a
pliris <nikich85@inbox.ru>
2020-05-04 23:10:25 +0300

    5.8. JavaDoc. Code convention[#283402]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,14 +1,39 @@
 package ru.job4j.converter;
+/**
+** Конвертер валют
+ * @author Nikita
+*/
 
 public class Converter {
+    /**
+     * Method rubleToEuro.
+     * @param value - количество рублей
+     * @return rsl - количество евро
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
+    /**
+     * Method rubleToDollar.
+     * @param value - количество рублей
+     * @return rsl - количество долларов
+     */
     public static int rubleToDollar(int value) {
         int rsl = value / 45;
         return rsl;
     }
+     /**
+     *Main
+     *@param args - euro - итоговое количество евро
+      * @param args - dollar - итоговое количество доллларов
+      * @param args - in - количество рублей
+      * @param args - expectedEuro - ожидаемое количество евро
+      * @param args - expectedDollar - ожидвемое количество долларов
+      * @param args - out - фактичееское количество валюты после вычислений
+      * @param args - passedEuro - истинность совпадения ожидаемого с фактическим в евро
+      * @param args - passedDollar - истинность совпадения ожидаемого с фактическим в долларах
+     */
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(140);
