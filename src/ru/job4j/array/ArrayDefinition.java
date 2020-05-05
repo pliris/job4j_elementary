4053760d613b1b5cbf4d55d3ff469da0c9dc4fef
pliris <nikich85@inbox.ru>
2020-05-05 19:46:35 +0300

    6.0.1. Объявление массива. [#283338]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,13 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[10500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Nikita";
+        names[1] = "Alexey";
+        names[2] = "Nina";
+        names[3] = "Lena";
+        for (int i = 0; i < 4; i++) {
+        System.out.println(names[i].toString());
+        }
     }
 }
