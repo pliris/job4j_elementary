29560fb5945f739430f13a45ffe23d638d7c81b0
pliris <nikich85@inbox.ru>
2020-05-14 14:22:46 +0300

    4. Расстояние между точками в трехмерном пространстве.[#283481]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -4,25 +4,44 @@ import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
 public class Point {
-
+    /**
+     * Это поле объекта. Оно доступно только кокретному объекту.
+     */
     private int x;
-
+    /**
+     * И это поле объекта. Оно доступно только кокретному объекту.
+     */
     private int y;
-
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private  int z;
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
-
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+    /**
+     * Метод, который считает дистанции между точками в трехмерном пространстве
+     * @param that точка до которой считаем расстояние
+     * @return возвращает расстояние
+     */
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
     public double distance(Point that) {
-
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
-
-    public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double dist = a.distance(b);
-        System.out.println(dist);
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -1,22 +1,24 @@
-//package ru.job4j.condition;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
-//
-//public class PointTest {
-//
-//    @Test
-//    public void whenPoint1244ThenDist() {
-//        int x1 = 1;
-//        int y1 = 2;
-//        int x2 = 4;
-//        int y2 = 4;
-//        double expected = 3.6;
-//        double out = Point.distance(x1,y1,x2,y2);
-//        Assert.assertEquals(expected, out, 0.01);
-//
-//
-//    }
-//}
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void whenPoint124435ThenDist() {
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 4;
+        int y2 = 4;
+        int z1 = 3;
+        int z2 = 5;
+        Point pointOne = new Point(x1, y1, z1);
+        Point pointTwo = new Point(x2, y2, z2);
+        double expected = 4.1;
+        double out = pointOne.distance3d(pointTwo);
+        Assert.assertEquals(expected, out, 0.1);
+    }
+}
