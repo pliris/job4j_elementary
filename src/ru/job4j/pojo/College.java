b15a051042bfd7024ffc6b9646dc3fb667c54b22
pliris <nikich85@inbox.ru>
2020-05-15 13:03:01 +0300

    2. Модель данных.[#283501]

---
+++ src/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Nikita");
+        student.setGroup("5");
+        student.setReciept("01.05.2020");
+        System.out.println(student.getFio() + " "+ student.getGroup() + " " + student.getReciept());
+    }
+}
