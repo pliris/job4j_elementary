ac07f8529a4e17a8b9d92e87c7d806653a8990be
pliris <nikich85@inbox.ru>
2020-05-22 14:27:20 +0300

    9.1 Рефакторинг теста @Before @After[33568#283438]

--- test/ru/job4j/strategy/PaintTest.java
+++ test/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -9,18 +11,25 @@ import java.util.StringJoiner;
 
 
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфер для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
-        new Paint().draw(new Square());
-        // проверяем результат вычисления
-        assertThat(
+            new Paint().draw(new Square());
+            assertThat(
                 new String(out.toByteArray()),
                 is(
                         new StringJoiner(System.lineSeparator())
@@ -32,7 +41,5 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
     }
 }
