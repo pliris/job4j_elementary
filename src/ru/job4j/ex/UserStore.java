52da58ff314e64f65bcd0944367e6acccc535169
pliris <nikich85@inbox.ru>
2020-05-26 12:54:44 +0300

    0.5. Иерархия исключений и множественный catch.[219368#283462]

--- src/ru/job4j/ex/UserStore.java
+++ src/ru/job4j/ex/UserStore.java
@@ -7,6 +7,7 @@ public class UserStore {
         for (User user : users) {
             if (user.getUsername().equals(login)) {
                 rsl = user;
+                break;
             }
         }
         if (rsl == null) {
@@ -17,8 +18,13 @@ public class UserStore {
 
     public static boolean validate(User user) throws UserInvalidException {
         boolean rsl;
-        if (user.isValid() && user.getUsername().length()  > 2) {
-            rsl = true;
+        if (user.isValid()) {
+            if (user.getUsername().length() > 2) {
+                rsl = true;
+            } else {
+                throw new UserInvalidException("User must be 3 or bigger character");
+            }
+
         } else {
             throw new UserInvalidException("User invalid");
         }
