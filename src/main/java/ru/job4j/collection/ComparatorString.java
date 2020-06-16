a0d89ebc82d1e3a4d8c2b56d7d5834304e4c01b1
pliris <nikich85@inbox.ru>
2020-06-16 10:29:34 +0300

    Контрольные вопросы.[10097#283411]

--- src/main/java/ru/job4j/collection/ComparatorString.java
+++ src/main/java/ru/job4j/collection/ComparatorString.java
@@ -1,5 +1,6 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -7,14 +8,18 @@ import java.util.TreeSet;
 public class ComparatorString {
 
     public String compareTo(String o) {
-        String compareString = null;
-        TreeSet<Character> characters = new TreeSet<>();
-        for (int i = 0; i < o.length(); i++) {
-            characters.add(o.charAt(i));
-        }
-        for (Character ch : characters) {
-            compareString = (compareString != null) ? compareString + ch : ch.toString();
-        }
+
+//        TreeSet<Character> characters = new TreeSet<>();
+//        for (int i = 0; i < o.length(); i++) {
+//            characters.add(o.charAt(i));
+//        }
+//        for (Character ch : characters) {
+//            compareString = (compareString != null) ? compareString + ch : ch.toString();
+//        }
+//        return compareString;
+        char[] chars = o.toCharArray();
+        Arrays.sort(chars);
+        String compareString = new String(chars);
         return compareString;
     }
 }
