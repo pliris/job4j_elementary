b27fb0f0b6a0865a9ae23df8da9d2b0eecbd81d4
plifis <nikich85@inbox.ru>
2020-06-26 21:01:34 +0400

    6. Функции высшего порядка [#283270]

--- src/main/java/ru/job4j/search/Person.java
+++ src/main/java/ru/job4j/search/Person.java
@@ -1,10 +1,32 @@
 package ru.job4j.search;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private String surname;
     private String phone;
     private String address;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return Objects.equals(name, person.name)
+                && Objects.equals(surname, person.surname)
+                && Objects.equals(phone, person.phone)
+                && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, phone, address);
+    }
 
     public Person(String name, String surname, String phone, String address) {
         this.name = name;
--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,9 +1,14 @@
 package ru.job4j.search;
 
+import ru.job4j.collection.Job;
+
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
@@ -16,12 +21,25 @@ public class PhoneDictionary {
      */
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
+        Predicate<Person> name = n -> n.getName().equals(key);
+        Predicate<Person> surname = s -> s.getSurname().equals(key);
+        Predicate<Person> phone = p -> p.getPhone().equals(key);
+        Predicate<Person> address = a -> a.getAddress().equals(key);
+        Predicate<Person> combine = name.or(surname).or(phone).or(address);
         for (Person person : persons) {
-            if (person.getAddress().contains(key) || person.getName().contains(key)
-                    || person.getPhone().contains(key) || person.getSurname().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
         return result;
+
+
+//        for (Person person : persons) {
+//            if (person.getAddress().contains(key) || person.getName().contains(key)
+//                    || person.getPhone().contains(key) || person.getSurname().contains(key)) {
+//                result.add(person);
+//            }
+//        }
     }
+
 }
