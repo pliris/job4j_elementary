b2e7111d4e9a1b1990cbabc6708d2a127a230627
pliris <nikich85@inbox.ru>
2020-07-29 11:44:20 +0300

    Упражнения: flatmap для Итератора

--- src/main/java/ru/job4j/stream/FlatIt.java
+++ src/main/java/ru/job4j/stream/FlatIt.java
@@ -7,12 +7,6 @@ public class FlatIt {
     public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
         final List<Integer> list = new ArrayList<>();
         Spliterator<Iterator<Integer>> spl = Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED);
-//        while (it.hasNext()) {
-//            Iterator iterator = it.next();
-//            while (iterator.hasNext()) {
-//                list.add((Integer) iterator.next());
-//            }
-//        }
         StreamSupport.stream(spl, false)
                 .spliterator()
                 .forEachRemaining(i -> i.forEachRemaining(ir -> list.add(ir)));
