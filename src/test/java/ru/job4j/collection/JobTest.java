fb41e83e9d2a89e3295415b93e451088882b53af
plifis <nikich85@inbox.ru>
2021-03-09 21:01:10 +0300

    JObTest

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -11,13 +11,13 @@ import static org.junit.Assert.*;
 public class JobTest {
 
     @Test
-    public void whenCompatorByName() {
+    public void whenComparatorByName() {
         Comparator<Job> sortJobNameAtoZ = new SortNameAtoZ();
-        List<Job> list = List.of(
+        List<Job> list = new ArrayList<>(List.of(
                 new Job("Fix bug", 1),
                 new Job("X task", 0),
                 new Job("Annotation", 4)
-        );
+        ));
         Collections.sort(list, sortJobNameAtoZ);
         List<Job> expected = List.of(
                 new Job("Annotation", 4),
@@ -28,14 +28,14 @@ public class JobTest {
     }
 
     @Test
-    public void whenCompatorByPrority() {
+    public void whenComparatorByPriority() {
         Comparator<Job> sortJobPriorityAtoZ = new SortPriorityAtoZ();
-        List<Job> list = List.of(
+        List<Job> list = new ArrayList<>(List.of(
                 new Job("Fix bug", 1),
                 new Job("X task", 0),
                 new Job("Annotation", 4)
-        );
-        Collections.sort(list, sortJobPriorityAtoZ);
+        ));
+        list.sort(sortJobPriorityAtoZ);
         List<Job> expected = List.of(
                 new Job("X task", 0),
                 new Job("Fix bug", 1),
@@ -45,7 +45,7 @@ public class JobTest {
     }
 
     @Test
-    public void whenCompatorByPriorityThenZToA() {
+    public void whenComparatorByPriorityThenZToA() {
         Comparator<Job> sortJobPriorityZtoA = new SortPriorityZtoA();
         int rsl = sortJobPriorityZtoA.compare(
                 new Job("Fix bug", 1),
@@ -56,7 +56,7 @@ public class JobTest {
     }
 
     @Test
-    public void whenCompatorByNameAndPriorityFromZToA() {
+    public void whenComparatorByNameAndPriorityFromZToA() {
         Comparator<Job> sortJobNamePriorityZtoA = new SortNameAtoZ().thenComparing(new SortPriorityZtoA());
         int rsl = sortJobNamePriorityZtoA.compare(
                 new Job("Fix bug", 1),
@@ -67,14 +67,14 @@ public class JobTest {
     }
 
     @Test
-    public void whenComparatorByPriorityandNameFromAToZ() {
+    public void whenComparatorByPriorityAndNameFromAToZ() {
         Comparator<Job> cmp = new SortPriorityAtoZ().thenComparing(new SortNameAtoZ());
-        List<Job> list = List.of(
+        List<Job> list = new ArrayList<>(List.of(
                 new Job("X task", 5),
                 new Job("Fix bug", 5),
                 new Job("Fix bug", 4),
                 new Job("AAA", 1)
-        );
+        ));
         List<Job> expected = List.of(
                 new Job("AAA", 1),
                 new Job("Fix bug", 4),
